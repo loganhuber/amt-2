f02a11dba83e436d6d687339b08d3b71fb781394
Logan <cjhuber102@mail.com>
2026-02-17 20:38:00 -0600

    initial commit

---
+++ routes.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import render_template, redirect, url_for
+
+
+
+def register_routes(app):
+    
+    @app.route("/")
+    def index():
+        return render_template('index.html')
