ed1d56536f12a2fa0e1266244a22aca50fb8c93a
Logan <cjhuber102@mail.com>
2026-02-23 14:00:24 -0600

    content to video page

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from routes import register_routes
+from routes_admin import register_admin_routes
 
 app = Flask(__name__)
 
 register_routes(app)
+register_admin_routes(app)
 
 if __name__=='__main__':
     app.run(debug=True)
---
+++ routes_admin.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import render_template, redirect, url_for
+
+
+def register_admin_routes(app):
+
+    @app.route('/dashboard')
+    def dashboard():
+        render_template('admin/dashboard.html')
+
+
+    @app.route('/login')
+    def login():
+        render_template('admin/login')
