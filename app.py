baf8c77f0393f10aa2956333d34408f2e899d654
Logan <cjhuber102@mail.com>
2026-03-05 22:08:36 -0600

    hid secret key

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import os
 app = Flask(__name__, instance_relative_config=True)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SECRET_KEY'] = 'my_secret'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
